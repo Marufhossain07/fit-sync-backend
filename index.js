fc648a6bba4d986c8d0a4618a1f10da34a45a4af
Marufhossain07 <marufprogrammer07@gmail.com>
2024-06-09 23:32:26 +0600

    applied get and patch method done

--- index.js
+++ index.js
@@ -8,13 +8,13 @@ const app = express();
 
 app.use(
     cors({
-      origin: [
-        "http://localhost:5173",
-        "https://cardoctor-bd.web.app",
-        "https://cardoctor-bd.firebaseapp.com",
-      ]
+        origin: [
+            "http://localhost:5173",
+            "https://cardoctor-bd.web.app",
+            "https://cardoctor-bd.firebaseapp.com",
+        ]
     })
-  );
+);
 app.use(express.json())
 
 
@@ -38,75 +38,107 @@ async function run() {
         const classesCollection = client.db('fitSync').collection('classes')
         const usersCollection = client.db('fitSync').collection('users')
         const trainersCollection = client.db('fitSync').collection('trainers')
-        app.post('/subscribe', async(req,res)=>{
+        app.post('/subscribe', async (req, res) => {
             const data = req.body;
             const result = await subscribersCollection.insertOne(data);
             res.send(result);
         })
 
-        app.get('/subscribe', async(req,res)=>{
+        app.get('/subscribe', async (req, res) => {
             const result = await subscribersCollection.find().toArray();
             res.send(result)
         })
-        app.delete('/subscribe/:id', async(req,res)=>{
+        app.delete('/subscribe/:id', async (req, res) => {
             const id = req.params.id;
-            const query = { _id: new ObjectId(id)}
+            const query = { _id: new ObjectId(id) }
             const result = await subscribersCollection.deleteOne(query)
             res.send(result)
         })
 
-        app.post('/add-class', async(req,res)=>{
+        app.post('/add-class', async (req, res) => {
             const data = req.body;
             const result = await classesCollection.insertOne(data);
             res.send(result)
         })
-        app.get('/classes', async(req,res)=>{
+        app.get('/classes', async (req, res) => {
             const result = await classesCollection.find().toArray()
             res.send(result)
         })
 
-        app.post('/users', async(req,res)=>{
+        app.post('/users', async (req, res) => {
             const newUser = req.body;
-            const query = {email: newUser.email}
+            const query = { email: newUser.email }
             const existingUser = await usersCollection.findOne(query);
-            if(existingUser){
+            if (existingUser) {
                 const updatedDoc = {
-                    $set : {
+                    $set: {
                         lastLogin: newUser.lastLogin
                     }
                 }
-                const result = await usersCollection.updateOne(query,updatedDoc)
-               return res.send({message:'user already exists', result})
+                const result = await usersCollection.updateOne(query, updatedDoc)
+                return res.send({ message: 'user already exists', result })
             }
             const result = await usersCollection.insertOne(newUser);
             res.send(result)
         })
 
-        app.post('/trainer', async(req,res)=>{
+        app.post('/trainer', async (req, res) => {
             const newTrainer = req.body;
             const result = await trainersCollection.insertOne(newTrainer);
             res.send(result)
         })
 
-        app.get('/trainer', async(req,res)=>{
-            const result = await trainersCollection.find().toArray()
+        app.get('/trainer', async (req, res) => {
+            const query = {status: 'accepted'}
+            const result = await trainersCollection.find(query).toArray()
             res.send(result)
         })
 
 
-        app.delete('/trainer', async(req,res)=>{
+        app.delete('/trainer', async (req, res) => {
             const id = req.query.id
             const email = req.query.email
-            const query = {_id: new ObjectId(id)}
+            const query = { _id: new ObjectId(id) }
+            const filter = { email: email }
+            const updatedDoc = {
+                $set: {
+                    role: 'member'
+                }
+            }
+            const updatedResult = await usersCollection.updateOne(filter, updatedDoc)
+            const result = await trainersCollection.deleteOne(query)
+            res.send({ result, updatedResult })
+        })
+
+
+        app.get('/applied', async (req, res) => {
+            const query = { status: 'pending' }
+            const result = await trainersCollection.find(query).toArray();
+            res.send(result)
+        })
+
+        app.get('/applied/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await trainersCollection.findOne(query);
+            res.send(result);
+        })
+        app.patch('/applied/:email', async(req,res)=>{
+            const email = req.params.email;
             const filter = {email: email}
             const updatedDoc = {
                 $set: {
-                    role: 'user'
+                    status: 'accepted'
                 }
             }
-            const updatedResult = await usersCollection.updateOne(filter,updatedDoc) 
-            const result = await trainersCollection.deleteOne(query)
-            res.send({result,updatedResult})
+            const updatedRole = {
+                $set: {
+                    role: 'trainer'
+                }
+            }
+            const roleResult = await usersCollection.updateOne(filter,updatedRole)
+            const result = await trainersCollection.updateOne(filter,updatedDoc)
+            res.send({result,roleResult})
         })
         // Send a ping to confirm a successful connection
         // await client.db("admin").command({ ping: 1 });
